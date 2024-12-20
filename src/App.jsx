9cce69d4f6d85d0279c1a45712ae77884d276042
andresfelipecespedesgomez@gmail.com <andresfelipecespedesgomez@gmail.com>
2024-12-20 15:02:57 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <div className="contenedor-calculadora">  
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
