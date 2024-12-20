fc100217149ad353ca95e1ee8348f003a09b5e36
andresfelipecespedesgomez@gmail.com <andresfelipecespedesgomez@gmail.com>
2024-12-20 15:50:32 -0500

    botones

---
+++ src/components/Boton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/Boton.css'
+
+export const Boton = (props) => {
+
+    const esOp=valor=>{
+        return isNaN(valor) && (valor != '.') && (valor != '=');
+    };
+
+
+    return (
+        <div className={`boton-contenedor ${esOp(props.children) ? 'operador' : null}`.trimEnd()}>
+            {props.children}
+        </div>
+    )
+}
